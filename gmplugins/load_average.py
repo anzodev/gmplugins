9d37aa8d561b6abf62e71d6739f392a6faf27288
anzodev <29258410+anzodev@users.noreply.github.com>
2021-08-29 14:06:38 +0300

    added load average representation in percent to load_average plugin tooltip

--- gmplugins/load_average.py
+++ gmplugins/load_average.py
@@ -6,10 +6,13 @@ from . import Plugin
 
 class LoadAverage(Plugin):
     def _make_output(self, _) -> Output:
+        loadavg = psutil.getloadavg()
+        cpu_count = psutil.cpu_count()
+
         icon = "\uf2db"  # fontawesome icon "microchip"
-        txt = " ".join([str(i) for i in psutil.getloadavg()])
+        txt = " ".join([str(i) for i in loadavg])
         txtclick = "xfce4-terminal -e 'top -d 1'"
-        tool = "Load average"
+        tool = " ".join([f"{x / cpu_count * 100:.2f}%" for x in loadavg])
         return Output(icon, txt, txtclick, tool)
 
 
